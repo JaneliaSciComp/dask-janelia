f90db0851cc71da8b7d905c18aeeb599d357df17
Davis Vann Bennett <davis.v.bennett@gmail.com>
2020-05-07 20:18:15 -0400

    add line to print dashboard URL

--- examples/example_script.py
+++ examples/example_script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from dask_janelia import autoClient
-from distributed import progress
+from distributed import progress, Client
 from pathlib import Path
 import os
 
@@ -13,7 +13,8 @@ def process_file(fname):
 
 
 if __name__ == "__main__":
-    with autoClient(threads_per_worker=1) as cl:
+    with autoClient(host="") as cl:
+        print(cl.cluster.dashboard_url)
         home_files = list(Path.home().glob("*"))
         futures = cl.map(process_file, home_files)
         progress(futures)
