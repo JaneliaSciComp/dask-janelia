f002237515086d059fb9ffe4e7f81c876604f115
Davis Vann Bennett <davis.v.bennett@gmail.com>
2020-12-08 16:13:41 -0500

    black

--- examples/example_script.py
+++ examples/example_script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from dask_janelia import auto_cluster
+from dask_janelia import get_cluster
 from distributed import progress, Client
 from pathlib import Path
 import os
@@ -8,6 +8,7 @@ import random
 import time
 
 num_workers = 2
+
 mean_runtime = 20
 
 
@@ -21,14 +22,17 @@ def process_file(fname, delay):
 
 if __name__ == "__main__":
     # wrapping the client in a context manager ensures that the client and the cluster get cleaned up
-    with Client(auto_cluster()) as cl:
+    with get_cluster() as clust, Client(clust) as cl:
         print(f"Cluster dashboard running at {cl.cluster.dashboard_link}")
         # add workers
         cl.cluster.scale(num_workers)
+
         # list all the files in the home directory
         home_files = list(Path.home().glob("*"))
+
         # ensure that the total runtime is ~mean_runtime
         delays = ((mean_runtime * num_workers) / len(home_files),) * len(home_files)
+
         # map the function `process_file` over the arguments `home_files` and `delays`
         # this returns a collection of futures
         futures = cl.map(process_file, home_files, delays)
